7b1813ba31d69847e6c492a3938ce71d70ac8c7c
noah <43697087+noahsterner@users.noreply.github.com>
2026-09-10 23:07:02 +0200

    fix: remove PermisionViewModel and made PermissionsGate recomposable

--- app/src/main/java/com/example/roboticapp/view/PermissionGate.kt
+++ app/src/main/java/com/example/roboticapp/view/PermissionGate.kt
@@ -1,68 +1,42 @@
 package com.example.roboticapp.view
 
 
-import android.Manifest
-import android.R
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.provider.Settings
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import com.example.roboticapp.viewmodel.PermissionViewModel
 
 @Composable
 fun PermissionsGate(
     permissions: List<String>,
-    viewModel: PermissionViewModel = PermissionViewModel(),
     content: @Composable () -> Unit
 ) {
-    val permissionQueue = viewModel.pendingDialogQueue
+    val permissionQueue = remember { mutableStateListOf(*permissions.toTypedArray()) }
 
     val launcher = rememberLauncherForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions(),
         onResult = { results ->
             permissions.forEach { permission ->
-                viewModel.onPermissionResult(
-                    permission = permission,
-                    isGranted = results[permission] == true
-                )
+                if (results[permission] == true) {
+                    permissionQueue.remove(permission)
+                }
             }
         }
     )
@@ -73,6 +47,8 @@ fun PermissionsGate(
     }
 
     if (permissionQueue.isNotEmpty()) {
+        Log.i("PermissionGate", "${permissionQueue.size}")
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -89,6 +65,7 @@ fun PermissionsGate(
 
         }
     } else {
+        Log.i("PermissionGate", "${permissionQueue.size}")
         content()
     }
 }
